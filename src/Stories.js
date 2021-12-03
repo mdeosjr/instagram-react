1798227f19196a2fab7ce3590640ad84966ab664
Moises Junior <m.junior@eq.ufrj.br>
2021-12-03 00:26:47 -0300

    Stories componentizado com array de objetos e props

--- src/Stories.js
+++ src/Stories.js
@@ -2,19 +2,22 @@ import Story from './Story'
 
 export default function Stories() {
     const story = [
-        {imagem:"public/assets/img/9gag.svg", usuario: "9gag"},
-        {imagem:"public/assets/img/meowed.svg", usuario: "9gag"},
-        {imagem:"public/assets/img/barked.svg", usuario: "barked"},
-        {imagem:"public/assets/img/nathanwpylestrangeplanet.svg", usuario: "nathanwpylestrangeplanet"},
-        {imagem:"public/assets/img/wawawicomics.svg", usuario: "wawawicomics"},
-        {imagem:"public/assets/img/respondeai.svg", usuario: "respondeai"},
-        {imagem:"public/assets/img/filomoderna.svg", usuario: "filomoderna"},
-        {imagem:"public/assets/img/memeriagourmet.svg", usuario: "memeriagourmet"}                                              
+        {imagem:"./assets/img/9gag.svg", usuario: "9gag"},
+        {imagem:"./assets/img/meowed.svg", usuario: "meowed"},
+        {imagem:"./assets/img/barked.svg", usuario: "barked"},
+        {imagem:"./assets/img/nathanwpylestrangeplanet.svg", usuario: "nathanwpylestrangeplanet"},
+        {imagem:"./assets/img/wawawicomics.svg", usuario: "wawawicomics"},
+        {imagem:"./assets/img/respondeai.svg", usuario: "respondeai"},
+        {imagem:"./assets/img/filomoderna.svg", usuario: "filomoderna"},
+        {imagem:"./assets/img/memeriagourmet.svg", usuario: "memeriagourmet"}                                              
     ];
 
     return (
         <div class="stories">
             {story.map(story => <Story imagem={story.imagem} usuario={story.usuario}/>)}
+            <div class="setinha">
+                <ion-icon name="chevron-forward-circle"></ion-icon>
+            </div>
         </div>
     )
 }
